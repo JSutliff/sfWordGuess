aacf3a3f286f0273d3802d46f14c9f331613eb86
JSutliff <sutliff.jason@gmail.com>
2018-12-13 10:43:57 -0800

    html content added

---
+++ script.js
@@ -0,0 +1,17 @@
+const wordList = [];
+let currentWord = '';
+let lettersGuessed = [];
+let remainingGuesses;
+
+// Raondomly select word from word list 
+  // assign selected word to currentWord
+  // display _ for each letter of currentWord
+
+// if remaingingGuesses > 0 
+  // check user guess against currentWord
+  // if currentWord includes userGuess 
+    // display userGuessed letter in place of _
+
+  // else remainingGuesses --
+
+  
