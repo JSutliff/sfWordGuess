351bb77911c23f825877ff20deb8177e4de6e6f4
JSutliff <sutliff.jason@gmail.com>
2018-12-14 17:39:26 -0800

    card layout added and domUpdate impletemented

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-const wordList = [];
+const wordList = [
+  'geary',
+  'lombard',
+  'hyde',
+  'broadway',
+  'columbus',
+  'kearny',
+  'chestnut',
+  'karl',
+  'hella', 
+  'octavia', 
+  'tenderloin', 
+  'polk', 
+  'marina',
+  'haight',
+  'castro',
+  'sunset',
+  'mission',
+  'soma',
+  'japantown',
+  'chinatown',
+  'tech',
+  'wharf',
+  'alcatraz',
+  'startup',
+  'uber',
+  'lyft',
+  'giants',
+  'rent',
+  'hyphy',
+  'dank'
+];
 let currentWord = '';
+let displayWord = ''
 let lettersGuessed = [];
-let remainingGuesses;
+let remainingGuesses = 5;
+let wins = 0;
+let losses = 0;
 
 // Raondomly select word from word list 
-  // assign selected word to currentWord
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+  
   // display _ for each letter of currentWord
+// for (let i = 0; i < currentWord.length; i++) {
+//   displayWord = displayWord += '_ ';
+// }
+
 
 // if remaingingGuesses > 0 
   // check user guess against currentWord
@@ -14,4 +55,19 @@ let remainingGuesses;
 
   // else remainingGuesses --
 
-  
+function handleDomUpdate() {
+
+  //display wins/losses
+  document.querySelector('#wins').textContent = wins;
+  document.querySelector('#losses').textContent = losses;
+
+  // assign selected word to currentWord
+  currentWord = wordList[getRandomInt(wordList.length)];
+
+  // display _ for letters of current word
+  document.querySelector('#currentWord').textContent = currentWord.split('').map(elem => {
+    return elem = '_ ';
+  }).join(' ');
+}
+
+handleDomUpdate();
